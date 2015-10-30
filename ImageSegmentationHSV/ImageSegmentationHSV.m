46e2d008426e495062c4378a5b13440cecc503a5
sx4452 <zb.benzhang@gmail.com>
2015-10-30 19:26:58 +0800

    20151030

--- ImageSegmentationHSV/ImageSegmentationHSV.m
+++ ImageSegmentationHSV/ImageSegmentationHSV.m
@@ -4,6 +4,11 @@ I = rgb2gray(I);
 I = im2double(I);
 I = 1.0 - I;
 se = strel('disk', 10);
-I_out = imtophat(I, se);
+I_new = imtophat(I, se);
+[row, col] = find(I_new>0.2);
+I_out = zeros(size(I));
+for i = 1:size(row)
+    I_out(row(i),col(i)) = 1.0;
+end
 imwrite(I_out,'book_out.jpg');
 end
